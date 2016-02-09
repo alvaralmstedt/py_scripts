9c03dd12fc669f5e0a015cfa95519b5dd19439c4
alvaralmstedt <alvar.almstedt@gmail.com>
2016-02-09 14:55:56 +0100

    Added support for more input fields to put as comments in GFF field 9

--- blast_to_gff.py
+++ blast_to_gff.py
@@ -4,7 +4,8 @@ from sys import argv
 import csv
 
 """
-Converts minimal (3 field) tab separated BED files into minimal (9 field) tab separated GFF files.
+Converts minimal (3 field or 5 field) tab separated BED/blast result files into minimal (9 field)
+tab separated GFF files.
 Usage: blast_to_gff.py <BED-infile> <GFF-outfile>
 By: Alvar Almstedt
 """
@@ -15,16 +16,20 @@ class Table(object):
         self.input_file_name = input_file_name
         self.output_file_name = output_file_name
 
+# This method reads the input blast result
     def read_result(self):
-
         with open(self.input_file_name, 'r') as in_file:
-            fieldnames = ["header", "start", "stop"]
+            global fields
+            fieldnames = ["header", "sstart", "sstop", "qstart", "qstop", "qheader"]
+
             blast_reader = csv.DictReader(in_file, delimiter='\t', fieldnames=fieldnames)
             file_contents=[]
             for i in blast_reader:
                 file_contents.append(i)
+            fields = len(file_contents[0])  # set fields variable to number of fields read from input
             return file_contents
 
+# This method writes the output gff file
     def write_gff(self):
 
         with open(self.output_file_name, 'w+') as gff_out:
@@ -36,18 +41,31 @@ class Table(object):
                 gff_writer.writerow({"header": str(i["header"]),
                                      "blastresult": "blast_result",
                                      "hit": "hit",
-                                     "start": min(i["start"], i["stop"]),
-                                     "stop": max(i["start"], i["stop"]),
+                                     "start": str(min(int(i["sstart"]), int(i["sstop"]))),
+                                     "stop": str(max(int(i["sstart"]), int(i["sstop"]))),
                                      "score": ".",
-                                     "orientation": self.orientation(i["start"], i["stop"]),
+                                     "orientation": self.orientation(i["sstart"], i["sstop"]),
                                      "period": ".",
-                                     "ID": "ID=%s;Parent=%s" % (i["header"] + "_%s" % iterator, i["header"])})
+                                     "ID": self.number_of_fields(fields, i, iterator)})
                 iterator += 1
 
+# This method sets the orientation of the sequence
     def orientation(self, start, end):
         if int(start) < int(end):
             return "+"
         return "-"
+
+# This method creates a different comment field depending on how many fields the input file contains. Feel free to
+# change the numbers if the output of field 9 looks weird.
+    def number_of_fields(self, amount, i, iterator):
+        if amount < 4:
+            return "ID=%s;Parent=%s" % (i["header"] + "_%s" % iterator, i["header"])
+        elif amount < 6:
+            return "ID=%s;Parent=%s;QueryStart=%s;QueryStop=%s" % (i["header"] + "_%s" % iterator, i["header"],
+                                                                   i["qstart"], i["qstop"])
+        elif amount < 10:
+            return "ID=%s;Parent=%s;QueryStart=%s;QueryStop=%s;QueryID=%s" % (i["header"] + "_%s" % iterator, i["header"],
+                                                                   i["qstart"], i["qstop"], i["qheader"])
 
 if __name__ == "__main__":
     infile = argv[1]
