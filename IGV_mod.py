1e06f613664a32d84dccb5213c44b93e7405dbff
Alvar Almstedt <alvar.almstedt@gmail.com>
2018-07-13 10:29:57 +0200

    IGV_mod updated to latest

--- IGV_mod.py
+++ IGV_mod.py
@@ -17,3 +17,22 @@ def igv_modification(user, infile):
         userfile.seek(0)
         userfile.truncate()
         userfile.writelines(lines_of_file)
+
+    with open("/medstore/IGV_Folders/igv/users/%s_igv_su.xml" % user, "r+") as userfile:
+        lines_of_file = userfile.readlines()
+        bam = os.path.basename(infile)
+        lines_of_file.insert(-2,
+                             '\t\t<Resource name="%s" path="http://medstore.sahlgrenska.gu.se:80/data/%s/%s" />\n' % (
+                                 bam, user, bam))
+        userfile.seek(0)
+        userfile.truncate()
+        userfile.writelines(lines_of_file)
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    user = argv[1]
+    infile = argv[2]
+
+    igv_modification(user, infile)
