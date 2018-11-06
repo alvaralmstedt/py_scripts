947ec6d251ce3bdfeb44dbf47da7df0cefc8cad6
Alvar Almstedt <alvar.almstedt@gmail.com>
2018-11-06 14:15:29 +0100

    some progress on the canvas app

--- run_canvas.py
+++ run_canvas.py
@@ -7,9 +7,28 @@ import vcf
 import subprocess
 import logging
 
-def precheck_inputs():
+class BadBamFileError(Exception):
+    pass
+class PeriodsInVcfError(Exception):
+    pass
 
 
+def precheck_inputs(bam, vcf, igvuser, outpath, runtype):
+    if not bam.endwith(".bam"):
+        raise BadBamFileError(f"The bam file supplied does not look like a bam file: {bam}")
+    vcf_reader = vcf.Reader(open(vcf, 'r'))
+    for record in vcf_reader:
+        if record.FILTER == None:
+            raise PeriodsInVcfError(f"This vcf: {vcf} uses periods for pass values instead of PASS. We don't want that.")
+    igvusers = os.listdir("/seqstore/webfolders/igv/users")
+    valid_user = False
+    for userfiles in igvusers:
+        if igvuser in userfiles:
+            valid_user = True
+    if valid_user == False:
+        raise IgvUserDoesNotExistError(f"The user '{igvuser}' does not seem to have an xml present on seqstore")
+    if os.path.isdir(outpath):
+        info.logging(f"The output path you have supplied: {outpath} does not yet exist. It will be created by canvas")
 
 def igv_modification(user, user_xml_path, infile, port):
     """
@@ -30,7 +49,8 @@ def main_canvas(bam, vcf, outpath, runtype):
     my_env = os.environ.copy()
     canvas_dll = "/apps/bio/software/canvas/1.38.0.1554/Canvas.dll"
     filter13 = "medstore/External_References/Canvas_CLC_HG19_Dataset/custom/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/filter13.bed"
-    
+    ploidy_file = determine_sex(vcf)
+
     if runtype.upper() == "TN" or runtype.upper() == "SOMATIC-WGS":
         run_type = "Somatic-WGS"
         logging.info(f"Canvas sample is being run as: {run_type}")
@@ -44,6 +64,19 @@ def main_canvas(bam, vcf, outpath, runtype):
 
 def create_seg():
     pass
+
+
+# This will determine the sex of the sample by looking if there are eny entries in the Y-chromosome in the normal vcf
+def determine_sex(vcf_normal):
+    vcf_reader = vcf.Reader(open(vcf_normal, 'r'))
+    for record in vcf_reader:
+        if "Y" in record.CHROM:
+            logging.info("The sex was determined to be MALE")
+            return "/apps/bio/software/canvas/male_hg19.vcf"
+    logging.info("The sex was determined to be FEMALE")
+    return "/apps/bio/software/canvas/female_hg19.vcf"
+
+
 
 if __name__ == "__main__":
 
@@ -63,4 +96,8 @@ if __name__ == "__main__":
     igv_user = args.user
     output_path = args.output
     runtype = runtype.args
-    logging.info(f"The input arguments were set to: bam={bam_file}, normal vcf={normal_vcf}, IGV user={igv_user}, output path={output_path}")
+    logging.info(f"The input arguments were set to: runtype={runtype} bam={bam_file}, normal vcf={normal_vcf}, IGV user={igv_user}, output path={output_path}")
+
+    ports = [8008, 80]
+
+    precheck_inputs(bam_file, normal_vcf, igv_user, output_path, runtype)
